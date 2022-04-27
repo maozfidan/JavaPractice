617860ea881b68850b3465dd10564e474d98ffa7
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-04-27 22:33:29 +0200

    27.04.2022

---
+++ src/ff.java
@@ -0,0 +1,24 @@
+public class ff {
+
+    public static void main(String[] args) {
+
+        byte max = Byte.MAX_VALUE;
+        System.out.println(max);
+
+        char a = 'w';
+
+        int sayi = (int) a;
+        System.out.println(sayi);
+
+        System.out.println("merhaba dunya\n"+
+                "\t veli");
+
+        int x=0;
+
+        while(x<30){
+
+            System.out.print(x+",");
+            x++;
+        }
+    }
+}
